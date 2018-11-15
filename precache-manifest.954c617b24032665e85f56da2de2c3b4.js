7abd1304a6bb57941e0f4e9572f742790aaa77c1
Mark Dhillon <mark.dhillon@tosense.com>
2018-11-15 11:28:39 -0800

    Updates

---
+++ precache-manifest.954c617b24032665e85f56da2de2c3b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "828fa723c537cb8011cc",
+    "url": "/poos-reviews/static/css/main.e7fbdbf1.chunk.css"
+  },
+  {
+    "revision": "828fa723c537cb8011cc",
+    "url": "/poos-reviews/static/js/main.828fa723.chunk.js"
+  },
+  {
+    "revision": "d0f6679d7731a5504e24",
+    "url": "/poos-reviews/static/js/1.d0f6679d.chunk.js"
+  },
+  {
+    "revision": "4499838acb0c9c68fdea",
+    "url": "/poos-reviews/static/js/runtime~main.4499838a.js"
+  },
+  {
+    "revision": "6494f4858cceaf31d0674df5d3170b64",
+    "url": "/poos-reviews/index.html"
+  }
+];
